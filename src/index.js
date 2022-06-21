3d2b137cec54809d30cc4968a51597b97e355ea4
Manoj Khatri <manoj.khatri@vesuvio.io>
2022-06-21 22:57:53 +0545

    Login Authentication with JWT Access, Refresh Tokens, Cookies and Axios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AuthProvider } from "./reactFormVaidation/context/AuthProvider";
+import { AuthProvider } from "./context/AuthProvider";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
