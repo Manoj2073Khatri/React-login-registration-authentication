5235712eca85c19e8d9b0de91cad4d899d94d166
Manoj Khatri <manoj.khatri@vesuvio.io>
2022-06-21 20:39:39 +0545

    user registration form with all rejex checks

---
+++ src/reactFormVaidation/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+import Register from './register'
+
+const ReactFormValidation = () => {
+  return (
+    <div className='App'>
+
+        <Register/>
+    </div>
+  )
+}
+
+export default ReactFormValidation
